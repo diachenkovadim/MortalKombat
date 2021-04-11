6872d679ef15a2aa407eafe6e71b7ba38cc24f1a
Vadim <vadim.dyachenko.2018@ukr.net>
2021-04-11 10:58:25 +0300

    HomeWork-3

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const divArenas = document.querySelector('.arenas');
+const randomButton = document.querySelector('.button');
+
 const firstPlayer = {
     name: 'Scorpion',
     hp: 100,
@@ -7,6 +10,7 @@ const firstPlayer = {
         'sword', 
         'nunchucks'
     ],
+    player: 1,
     attack: function () {
         console.log(`${this.name} Fight`)
     }
@@ -21,57 +25,84 @@ const secondPlayer = {
         'sword', 
         'nunchucks'
     ],
+    player: 2,
     attack: function () {
         console.log(`${this.name} Fight`)
     }
 }
 
+function createElement (tag, className) {
+    const $tag = document.createElement(tag);
+    if (className) {
+        $tag.classList.add(className);
+    }
+    return $tag
+}
+
+function changeHp (player) {
+    const playerLife = document.querySelector(`.player${player.player} .life`)
+    player.hp -= Math.ceil(Math.random() * 20);
+
+    if (player.hp <= 0) {
+        player.hp = 0;
+        randomButton.disabled = true;
+    } 
+
+    playerLife.style.width = `${player.hp}%`;
+}
+
+randomButton.addEventListener('click', function () {
+    console.log('click');
+    console.log('first player: ', firstPlayer.hp);
+    console.log('second player: ', secondPlayer.hp);
+    changeHp(firstPlayer);
+    changeHp(secondPlayer);
+    showWinner(firstPlayer, secondPlayer)
+
+})
+
+function playerWin (name) {
+    const winTitle = createElement('div', 'winTitle');
+    winTitle.innerText = `${name} wins`;
+    return winTitle;
+}
+
+function showWinner (playerName1, playerName2) {
+    if (randomButton.disabled && playerName1.hp > playerName2.hp) {
+            divArenas.appendChild(playerWin(playerName1.name));
+         } else if (randomButton.disabled && playerName2.hp > playerName1.hp) {
+            divArenas.appendChild(playerWin(playerName2.name));
+         }
+}
+
 function createPlayer (playerClass, playerProperties) {
     // divPlayer
-    const divPlayer1 = document.createElement('div')
-    divPlayer1.classList.add(playerClass);
- 
+    const divPlayer1 = createElement('div', playerClass);
     // progress Bar
-    const divProgressBar = document.createElement('div')
-    divProgressBar.classList.add('progressbar');
-    
-    const divLife = document.createElement('div')
-    divLife.classList.add('life')
+    const divProgressBar = createElement('div', 'progressbar');
+    // div Life
+    const divLife = createElement('div', 'life')
     divLife.style.width = `${playerProperties.hp}%`;
     divProgressBar.appendChild(divLife);
-
      // div name
-    const divName = document.createElement('div')
-    divName.classList.add(playerProperties.name);
+    const divName = createElement('div', 'name');
     divName.innerText = playerProperties.name;
 
     divProgressBar.appendChild(divName);
     divPlayer1.appendChild(divProgressBar);
     
     // div character
-    const divCharacter = document.createElement('div')
-    divCharacter.classList.add('character');
-    
+    const divCharacter = createElement('div', 'character');
     // div img
-    const $image = document.createElement('img');
+    const $image = createElement('img');
     $image.src = playerProperties.img;
     divCharacter.appendChild($image);
 
     divPlayer1.appendChild(divCharacter);
-    // div Arenas
-<<<<<<< Updated upstream
-    const divArenas = document.createElement('div');
-    divArenas.classList.add('arenas');
-=======
-    const divArenas = document.querySelector('.arenas');
->>>>>>> Stashed changes
-    divArenas.appendChild(divPlayer1);
     
+    return divPlayer1;
 }
 
-createPlayer('player1', firstPlayer);
-<<<<<<< Updated upstream
-createPlayer('player2', secondPlayer);
-=======
-createPlayer('player2', secondPlayer);
->>>>>>> Stashed changes
+divArenas.appendChild(createPlayer('player1', firstPlayer));
+divArenas.appendChild(createPlayer('player2', secondPlayer));
+
